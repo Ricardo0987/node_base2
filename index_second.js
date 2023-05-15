ce7908843adb25dcc1de63136edd7b3277a6d898
Ricardo Ospina <ricardo.ospina@qrvey.com>
2023-05-14 20:37:11 -0500

    ...

--- index_second.js
+++ index_second.js
@@ -1,19 +1,18 @@
-console.log("test 2");
 require("dotenv").config();
 
 console.log(process.env);
 
-const figlet = require("figlet");
+// const figlet = require("figlet");
 
-const myVar = process.env.VAR_REPO_2;
-console.log(
-  figlet.textSync(myVar, {
-    //   font: "Ghost",
-    horizontalLayout: "default",
-    verticalLayout: "default",
-    width: 80,
-    whitespaceBreak: true,
-  })
-);
+// const myVar = process.env.VAR_REPO_2;
+// console.log(
+//   figlet.textSync(myVar, {
+//     //   font: "Ghost",
+//     horizontalLayout: "default",
+//     verticalLayout: "default",
+//     width: 80,
+//     whitespaceBreak: true,
+//   })
+// );
 
 console.log("DATA_RECEIVED:", process.env.DATA_RECEIVED);
